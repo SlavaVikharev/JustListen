84692d96f0974d38ec1927438180dff801cd5b53
slavavikharev <slavavicharev@gmail.com>
2015-12-05 00:32:46 +0500

    Highlighting the walls and non scalable on mobile phones now

--- index.js
+++ index.js
@@ -5,12 +5,13 @@
         this.level = 1;
         this.ball = new Ball(this.level);
         this.target = new Target(this.level);
+        this.walls = [new Wall(0), new Wall(1), new Wall(2), new Wall(3)];
     };
 
     Game.prototype.nextLevel = function() {
         var x = canvas.width * (this.target.x - this.ball.x) / 100;
         var y = canvas.height * (this.target.y - this.ball.y) / 100;
-        var r = canvas.height * this.target.radius / 100;
+        var r = Math.min(canvas.height, canvas.width) * this.target.radius / 100;
         if (x * x + y * y > r * r) {
             this.score = 0;
             this.level = 1;
@@ -36,6 +37,15 @@
     Ball.prototype.move = function(self) {
         self.x += self.vector[0] * self.speed;
         self.y += self.vector[1] * self.speed;
+        if (self.x < 0) {
+            game.walls[0].ttl = 10;
+        } else if (self.y < 0) {
+            game.walls[1].ttl = 10;
+        } else if (self.x > 100) {
+            game.walls[2].ttl = 10;
+        } else if (self.y > 100) {
+            game.walls[3].ttl = 10;
+        }
         if (self.x > 100 || self.x < 0 || self.y > 100 || self.y < 0) {
             self.vector = [-self.vector[0], -self.vector[1]];
             hit.play();
@@ -66,25 +76,57 @@
         ctx.beginPath();
         ctx.arc(canvas.width * this.x / 100,
                 canvas.height * this.y / 100,
-                this.radius * canvas.height / 100,
+                this.radius * Math.min(canvas.height, canvas.width) / 100,
                 0, 2 * Math.PI, false);
         ctx.stroke();
         ctx.lineWidth = 1;
         ctx.strokeStyle = '#000';
-    }
+    };
+
+    var Wall = function(side) {
+        this.side = side;
+        this.ttl = 0;
+    };
+
+    Wall.prototype.draw = function() {
+        if (!this.ttl) {
+            return;
+        }
+        var down = canvas.height;
+        var right = canvas.width;
+        ctx.fillStyle = '#f32';
+        switch (this.side) {
+            case 0:
+                ctx.fillRect(0, 0, 6, down);
+                break;
+            case 1:
+                ctx.fillRect(0, 0, right, 6);
+                break;
+            case 2:
+                ctx.fillRect(right - 6, 0, right, down);
+                break;
+            case 3:
+                ctx.fillRect(0, down - 6, right, down);
+                break;
+        }
+        ctx.fillStyle = '#000';
+        this.ttl--;
+    };
 
     var canvas = document.getElementById('game_canvas');
     var ctx = canvas.getContext("2d");
+
     var fps = 60;
 
     var game = new Game();
     var hit = new Audio('hit.mp3');
-    hit.play();    
+    hit.play();
 
     resizeCanvas();
     requestAnimationFrame(draw);
     window.addEventListener('resize', resizeCanvas);
     window.addEventListener('keydown', onKeyDown);
+    window.addEventListener('click', onClicked)
 
     function draw() {
         setTimeout(function() {
@@ -93,30 +135,39 @@
             ctx.fillRect(0, 0, canvas.width, canvas.height);
             ctx.fillStyle = '#000';
             game.target.draw();
-            // game.ball.draw();
+            game.ball.draw();
             ctx.font = "30px Tahoma";
             ctx.fillStyle = '#fff';
             ctx.fillText(game.score, 100, 100);
             ctx.fillStyle = '#000';
+            for (var i = 0; i < game.walls.length; i++) {
+                game.walls[i].draw();
+            }
         }, 1000 / fps);
     }
     
     function onKeyDown(e) {
         if (e.keyCode == 32) {
-            if (game.ball.goesUp) {
-                clearInterval(game.ball.moving);
-                game.nextLevel();
-            } else {
-                game.ball.goesUp = true;
-                game.ball.vector = [0, -1];
-                hit.play();
-            }
+            onClicked();
         }
     }
+
+    function onClicked() {
+        if (game.ball.goesUp) {
+            clearInterval(game.ball.moving);
+            game.nextLevel();
+        } else {
+            game.ball.goesUp = true;
+            game.ball.vector = [0, -1];
+            hit.play();
+        }
+    }
+ 
+    canvas.addEventListener("click",fullscreen)
     
     function resizeCanvas() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+        canvas.width = document.body.clientWidth;
+        canvas.height = document.body.clientHeight;
     }
 
 })();
